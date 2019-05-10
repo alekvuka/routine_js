5135224e12532618f214527ebc85b4e2302c7113
github username <github email address>
2019-05-09 23:47:34 -0400

    create columns for user, routine and task tables

--- db/migrate/20190510034407_create_tasks.rb
+++ db/migrate/20190510034407_create_tasks.rb
@@ -2,6 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
 
+      t.string :name
+
       t.timestamps
     end
   end
--- db/migrate/20190510034415_create_routines.rb
+++ db/migrate/20190510034415_create_routines.rb
@@ -2,6 +2,12 @@ class CreateRoutines < ActiveRecord::Migration[5.0]
   def change
     create_table :routines do |t|
 
+      t.string :name
+      t.string :start_time
+      t.string :end_time
+
+      t.integer :originator_id
+
       t.timestamps
     end
   end
